006636f61118dd80e7f0bf62f5dfc8f51b39f42f
solodimeema <e.nunezguerra@ugto.mx>
2022-12-05 11:18:48 -0600

    Ajuste de registro de usuarios e inicio de sesion

--- src/verifyL.php
+++ src/verifyL.php
@@ -1,22 +1,20 @@
 <?php
-    $name = $_POST['nombre'];
     $usuario = $_POST['usuario'];
     $password = $_POST['contraseña'];
-    $puesto = $_POST['ocupacion'];
 
     session_start();
     $_SESSION['usuario'] = $usuario;  
     $conexion = mysqli_connect("Database","root","MYSQL_ROOT_PASSWORD","DatabaseDS");
     
     if(isset($_POST['btnLogin'])){
-        $resultado = mysqli_query($conexion,"SELECT Nombre,username,Contraseña,Cargo FROM Usuarios WHERE username='$usuario' and Contraseña='$password'");
+        $resultado = mysqli_query($conexion,"SELECT Nombre,username,Contraseña,Cargo FROM Usuarios WHERE username='$usuario'");
         $nr = mysqli_num_rows($resultado);
         $filas = mysqli_fetch_array($resultado);
-        
-        if(($filas == 1)&&(password_verify($password,$filas['contraseña']))){
-          header('Location: ../page/admin.php');
-        }elseif($filas['Cargo'] == 2){
-          header('Location: ../page/cliente.php');
+              
+        if(($filas['Cargo'] == 1)&&(password_verify($password,$filas['Contraseña']))){
+          include('page/admin.php');
+        }elseif(($filas['Cargo'] == 2)&&(password_verify($password,$filas['Contraseña']))){
+          include('page/cliente.php');
         }else{
           ?>
           <div class="alert alert-warning" role="alert">
--- src/verifyS.php
+++ src/verifyS.php
@@ -1,7 +1,16 @@
 <?php
+    $name = $_POST['nombre'];
+    $usuario = $_POST['usuario'];
+    $password = $_POST['contraseña'];
+    $ocupacion = $_POST['ocupacion'];
+
+    session_start();
+    $_SESSION['usuario'] = $usuario;  
+    $conexion = mysqli_connect("Database","root","MYSQL_ROOT_PASSWORD","DatabaseDS");
+    
     if(isset($_POST['btnSignup'])){
         $password_sec = password_hash($password,PASSWORD_BCRYPT);
-        $registrar = "INSERT INTO Usuarios(Nombre,username,Contraseña,Cargo) VALUES('$name','$usuario','$password_sec','$puesto')";
+        $registrar = "INSERT INTO Usuarios(Nombre,username,Contraseña,Cargo) VALUES('$name','$usuario','$password_sec','$ocupacion')";
 
         if(mysqli_query($conexion,$registrar)){
             echo "<script>alert('Usuario registrado: $name');</script>";
